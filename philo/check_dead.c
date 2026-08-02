3f27d3f1822cfe503e3a299f58c1d4d9ec5861c4
George O <georgyostr@gmail.com>
2026-08-02 17:11:12 +0900

    norm and mutex restructure of print

--- philo/check_dead.c
+++ philo/check_dead.c
@@ -16,12 +16,12 @@ bool	check_dead(t_philo *philo)
 {
 	struct timeval	curr;
 
-	if (get_relative_time(philo, &curr))
-		return (true);
 	pthread_mutex_lock(philo->finish);
 	if (*philo->sim_finished)
 		return (pthread_mutex_unlock(philo->finish), true);
 	pthread_mutex_unlock(philo->finish);
+	if (get_relative_time(philo, &curr))
+		return (true);
 	if (time_more_eq(&curr, &philo->death_time))
 		return (print_status(philo, DIED), true);
 	return (false);
--- philo/print_status.c
+++ philo/print_status.c
@@ -12,44 +12,29 @@
 
 #include "philo.h"
 
+static bool	print_death(t_philo *philo, struct timeval *curr);
+static void	print_eating(t_philo *philo, struct timeval *curr);
+
 bool	print_status(t_philo *philo, enum e_status stat)
 {
 	struct timeval	curr;
 
 	pthread_mutex_lock(philo->finish);
 	if (*philo->sim_finished)
-		return (pthread_mutex_unlock(philo->finish), false);
+		return (pthread_mutex_unlock(philo->finish), true);
 	pthread_mutex_unlock(philo->finish);
-	if (get_relative_time(philo, &curr))
+	if (get_relative_time(philo, &curr) || print_death(philo, &curr))
 		return (true);
 	pthread_mutex_lock(philo->write);
-	if (time_more_eq(&curr, &philo->death_time))
-	{
-		pthread_mutex_lock(philo->finish);
-		*philo->sim_finished = true;
-		printf("%ld%03ld %u died\n", curr.tv_sec, curr.tv_usec / 1000,
+	if (curr.tv_sec == 0)
+		printf("%ld %u ", curr.tv_usec / 1000, philo->philo_num);
+	else
+		printf("%ld%03ld %u ", curr.tv_sec, curr.tv_usec / 1000,
 			philo->philo_num);
-		return (pthread_mutex_unlock(philo->write),
-			pthread_mutex_unlock(philo->finish), true);
-	}
-	printf("%ld%03ld %u ", curr.tv_sec, curr.tv_usec / 1000, philo->philo_num);
 	if (stat == SLEEPING)
 		printf("is sleeping");
 	else if (stat == EATING)
-	{
-		philo->eat_count += 1;
-		time_copy(&philo->death_time, &curr);
-		time_add(&philo->death_time, &philo->args->time_to_die);
-		if (philo->eat_count == philo->args->number_of_eat_to_finish)
-		{
-			pthread_mutex_lock(philo->finish);
-			*philo->eat_enough_count += 1;
-			if (*philo->eat_enough_count == philo->args->number_of_philos)
-				*philo->sim_finished = true;
-			pthread_mutex_unlock(philo->finish);
-		}
-		printf("is eating");
-	}
+		print_eating(philo, &curr);
 	else if (stat == THINKING)
 		printf("is thinking");
 	else if (stat == TAKEN_FORK)
@@ -57,4 +42,41 @@ bool	print_status(t_philo *philo, enum e_status stat)
 	printf("\n");
 	pthread_mutex_unlock(philo->write);
 	return (false);
+}
+
+static bool	print_death(t_philo *philo, struct timeval *curr)
+{
+	if (time_more_eq(curr, &philo->death_time))
+	{
+		pthread_mutex_lock(philo->finish);
+		if (*philo->sim_finished)
+		{
+			pthread_mutex_lock(philo->write);
+			if (curr->tv_sec == 0)
+				printf("%ld %u died\n", curr->tv_usec / 1000, philo->philo_num);
+			else
+				printf("%ld%03ld %u died\n", curr->tv_sec, curr->tv_usec / 1000,
+					philo->philo_num);
+			pthread_mutex_unlock(philo->write);
+		}
+		*philo->sim_finished = true;
+		return (true);
+	}
+	return (false);
+}
+
+static void	print_eating(t_philo *philo, struct timeval *curr)
+{
+	philo->eat_count += 1;
+	time_copy(&philo->death_time, curr);
+	time_add(&philo->death_time, &philo->args->time_to_die);
+	if (philo->eat_count == philo->args->number_of_eat_to_finish)
+	{
+		pthread_mutex_lock(philo->finish);
+		*philo->eat_enough_count += 1;
+		if (*philo->eat_enough_count == philo->args->number_of_philos)
+			*philo->sim_finished = true;
+		pthread_mutex_unlock(philo->finish);
+	}
+	printf("is eating");
 }
